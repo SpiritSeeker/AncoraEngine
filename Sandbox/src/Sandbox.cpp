9d0d682c1589d98cf3e7f288a1b264b657d16cb6
SpiritSeeker <sanjeet029@gmail.com>
2020-04-01 00:48:17 +0530

    Shaders: Added reading from file support.

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -89,40 +89,7 @@ public:
 
 		m_Shader.reset(Ancora::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string textureVertexSrc = R"(
-			#version 450 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string textureFragmentSrc = R"(
-			#version 450 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(Ancora::Shader::Create(textureVertexSrc, textureFragmentSrc));
+		m_TextureShader.reset(Ancora::Shader::Create("Sandbox/assets/shaders/Texture.glsl"));
 
 		std::string blackVertexSrc = R"(
 			#version 450 core
