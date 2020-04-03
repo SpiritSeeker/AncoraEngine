99ab18490b31358ea218f8bb948ee28d3f49185b
SpiritSeeker <sanjeet029@gmail.com>
2020-04-03 23:11:19 +0530

    OpenGL: Added OpenGL 4.5 Core Profile restriction.

--- Ancora/src/Ancora/Core/EntryPoint.h
+++ Ancora/src/Ancora/Core/EntryPoint.h
@@ -6,7 +6,6 @@ int main(int argc, char** argv)
 {
 	Ancora::Log::Init();
 	AE_CORE_WARN("Initialized Ancora!");
-	AE_INFO("Hello!");
 
 	Ancora::Application* app = Ancora::CreateApplication();
 	app->Run();
